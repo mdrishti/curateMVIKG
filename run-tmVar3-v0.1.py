f860479a0ba7316e9a971e4e2993388d9f881596
Disha Tandon <dishatandon.vit@gmail.com>
2025-08-28 12:07:25 +0100

    code to run tmVar3 on PMIDs

---
+++ run-tmVar3-v0.1.py
@@ -0,0 +1,83 @@
+import os
+import csv
+import sys
+import argparse
+import time
+import requests
+from pathlib import Path
+from typing import Optional, List
+
+REQUEST_DELAY = 0.34  # ~3 requests/sec
+_last_request_time = 0.0
+USER_AGENT = {"User-Agent": "pmc-downloader/1.0 (+https://example.org)"}
+
+## argument parser
+def parse_args():
+    parser = argparse.ArgumentParser(description="Get the tmVar3 annotations for PMC IDs")
+    parser.add_argument("-i", "--input", required=True, help="Path to CSV file with PMCID column")
+    parser.add_argument("-o", "--output", required=True, help="Directory to save extracted files")
+    parser.add_argument("--ignore-errors", action="store_true", help="Continue on errors")
+    return parser.parse_args()
+
+# common (specify PMCID or PID)
+def read_pmcids(csv_path: str) -> List[str]:
+    # note that utf-8-sig encoding is important her
+    # because otherwise the Byte Order Mark (BOM) is returned
+    # which gives you an idea of how to encode the rest of the file. 
+    # in this case it is \ufeffPMID, which means the key does 'PMID'
+    # does not work as intended with only utf-8 encoding. hence, utf-8-sig!
+    with open(csv_path, newline="", encoding="utf-8-sig") as f:
+        reader = csv.DictReader(f)
+        #for row in reader:
+        #    print(row)
+        return [row.get("PMID", "").strip() for row in reader if row.get("PMID", "").strip()]
+
+
+def tmVar3_endpoint(pmid: str) -> str:
+    return f"https://www.ncbi.nlm.nih.gov/research/pubtator3-api/publications/export/biocxml?pmids={pmid}&full=true"
+
+
+## delay in requests
+def throttle_request() -> None:
+    global _last_request_time
+    now = time.time()
+    elapsed = now - _last_request_time
+    if elapsed < REQUEST_DELAY:
+        time.sleep(REQUEST_DELAY - elapsed)
+    _last_request_time = time.time()
+
+
+def download_from_tmVar3(pmcid: str, output_dir: str, ignore_errors: bool) -> None:
+    pmc_folder = os.path.join(output_dir, f"{pmcid}.bioC")
+    if os.path.exists(pmc_folder):
+        print(f"{pmcid}: already exists")
+        return
+    url = tmVar3_endpoint(pmcid)
+    try:
+        throttle_request()
+        resp = requests.get(url, stream=True, timeout=120, headers=USER_AGENT)
+        resp.raise_for_status()
+        content_type = resp.headers.get("Content-Type", "")
+        with open(pmc_folder, "wb") as f2:
+            for chunk in resp.iter_content(chunk_size=1024*1024):
+                if chunk:
+                    f2.write(chunk)
+    except Exception as e:
+        print(f"{pmcid}: error - {e}")
+        if not ignore_errors:
+            raise
+
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    pmcids = read_pmcids(args.input)
+    #print(pmcids)
+    os.makedirs(args.output, exist_ok=True)
+    for pmc in pmcids:
+        print(pmc)
+        throttle_request()
+        download_from_tmVar3(pmc, args.output, args.ignore_errors)
+
+
+
